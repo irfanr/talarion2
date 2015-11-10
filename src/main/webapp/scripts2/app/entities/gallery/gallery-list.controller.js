ec4483b05889c57dd9542701f86ae041aaf7788f
irfanr <irfan.romadona@gmail.com>
2015-11-10 15:23:30 +0700

    Added More button

--- src/main/webapp/scripts2/app/entities/gallery/gallery-list.controller.js
+++ src/main/webapp/scripts2/app/entities/gallery/gallery-list.controller.js
@@ -3,9 +3,13 @@
 angular.module('talarion2App')
     .controller('GalleryListController', function($scope, $state, Image, ParseLinks, growl) {
 
-        $scope.imageList = {};
+        $scope.page = 0;
+
+        $scope.imageList = [];
 
         $scope.callServer = function callServer(tableState) {
+
+            $scope.page = $scope.page + 1;
 
             $scope.isLoading = true;
 
@@ -14,7 +18,7 @@ angular.module('talarion2App')
             // var start = pagination.start || 0; // This is NOT the page number, but the index of item in the list that you want to use to display the table.
             // var number = pagination.number || 10; // Number of entries showed per page.
             // var numberOfPages = pagination.numberOfPages || 1;
-            var page =0;
+            // $scope.page = 0;
 
             var nameSearchCrit = '';
 
@@ -23,16 +27,31 @@ angular.module('talarion2App')
             // }
 
             Image.query({
-                page: page,
+                page: $scope.page,
                 size: 12,
                 name: nameSearchCrit
             }, function(result, headers) {
                 $scope.links = ParseLinks.parse(headers('link'));
-                $scope.imageList = result;
-                tableState.pagination.numberOfPages = 1 * headers('X-Total-Pages');
-                tableState.pagination.number = 1 * headers('X-Size');
-                tableState.pagination.start = tableState.pagination.number * (page - 1);
+
+                // $scope.imageList.push.apply($scope.imageList, result);\
+
+                angular.forEach(result, function(newImage) {
+                    $scope.imageList.push(newImage);
+                });
+
+                // $scope.imageList = result;
+                // tableState.pagination.numberOfPages = 1 * headers('X-Total-Pages');
+                // tableState.pagination.number = 1 * headers('X-Size');
+                // tableState.pagination.start = tableState.pagination.number * (page - 1);
                 $scope.isLoading = false;
+
+                $scope.totalPages = headers('X-Total-Pages');
+
+                console.log('X-Total-Count: ' + headers('X-Total-Count'));
+                console.log('X-Number-Of-Elements:' + headers('X-Number-Of-Elements'));
+                console.log('X-Total-Pages:' + headers('X-Total-Pages'));
+                console.log('X-Number:' + headers('X-Number'));
+                console.log('X-Size:' + headers('X-Size'));
 
                 // console.log('tableState.pagination.start: ' + tableState.pagination.start);
                 // console.log('tableState.pagination.numberOfPages: ' + tableState.pagination.numberOfPages);
